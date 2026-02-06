86a4f9063a5ebd89f496a67890142ac9454551fd
i-ganza007 <i.ganza@alustudent.com>
2026-02-06 13:45:06 +0200

    feat: commiting modules & dockerFiles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AnimalsModule } from './animals/animals.module';
+import { BreedingEventsModule } from './breeding_events/breeding_events.module';
+import { BreedingRecModule } from './breeding_rec/breeding_rec.module';
+import { PrismaServiceModule } from './prisma-service/prisma-service.module';
+import { JwtserviceModule } from './jwtservice/jwtservice.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AnimalsModule, BreedingEventsModule, BreedingRecModule, PrismaServiceModule, JwtserviceModule],
   controllers: [AppController],
   providers: [AppService],
 })
