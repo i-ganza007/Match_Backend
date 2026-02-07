56481ca76086fba48830a9124e700e4530b94fd9
i-ganza007 <i.ganza@alustudent.com>
2026-02-07 03:12:47 +0200

    feat: ongoing db setup & readme setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AnimalsModule } from './animals/animals.module';
 import { BreedingEventsModule } from './breeding_events/breeding_events.module';
-import { BreedingRecModule } from './breeding_rec/breeding_rec.module';
 import { PrismaServiceModule } from './prisma-service/prisma-service.module';
 import { JwtserviceModule } from './jwtservice/jwtservice.module';
+import {ConfigModule} from '@nestjs/config'
+import path from 'node:path';
 
 @Module({
-  imports: [UsersModule, AnimalsModule, BreedingEventsModule, BreedingRecModule, PrismaServiceModule, JwtserviceModule],
+  imports: [UsersModule, AnimalsModule, BreedingEventsModule, PrismaServiceModule, JwtserviceModule,ConfigModule.forRoot({isGlobal:true,envFilePath:path.join(__dirname,'..','.env')})],
   controllers: [AppController],
   providers: [AppService],
 })
