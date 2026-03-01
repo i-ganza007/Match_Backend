2df2fc72e35140c8a258f76068e679fb75256823
i-ganza007 <i.ganza@alustudent.com>
2026-03-01 17:50:42 +0200

    fix: changing the schema folder

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "src/prisma/schema.prisma",
+  schema: "src/prisma/",
   migrations: {
     path: "src/prisma/migrations",
   },
